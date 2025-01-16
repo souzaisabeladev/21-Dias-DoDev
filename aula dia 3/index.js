751dcc0a59e4a4beeb7820a6c3ff22e9bb60f93b
souzaisabeladev <isainuyasha@icloud.com>
2025-01-15 21:37:59 -0300

    terceira aula teste

---
+++ aula dia 3/index.js
@@ -0,0 +1,18 @@
+/*tipos de texto e numero, e tipos de bool texto e bool */
+
+
+let tipoTexto = "2";
+let Tiponumber = 2;
+let TipoBoolTexto = "true";
+let TipoBool = true;
+
+
+
+/* tipos de variaveis */
+
+// let e const 
+let meuNome = 'Isabela Souza'
+console.log(meuNome)
+meuNome = 'Souza Isabela'
+console.log(meuNome)
+
