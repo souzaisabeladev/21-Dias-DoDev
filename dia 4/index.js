8d792698acbe050aa838e6844840af82fbc6715e
souzaisabeladev <isainuyasha@icloud.com>
2025-05-24 23:52:33 -0300

    aula sobre if

---
+++ dia 4/index.js
@@ -0,0 +1,22 @@
+let condicao = true;
+// ==igual 
+// != diferente
+// < menor
+// > maior
+// ===   é igual o valor e o tipo 
+// !== é diferente o valor e o tipo 
+
+if(condicao){
+console.log('entro no if')
+} else{
+    console.log('entro no else')
+}
+
+
+let idade = Number(prompt('insira a sua idade'))
+if(idade <= 10){
+console.log('Você tem menos ou tem 10 anos')
+
+}else{
+    console.log('você tem mais que 10 anos')
+}
